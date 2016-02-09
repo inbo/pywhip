5f9b295e12c22266e6fe5c2d0ff0e46731f7427b
stijnvanhoey <stijnvanhoey@gmail.com>
2016-02-09 18:14:04 +0100

    added basic sample feature to dict setup

--- loganomalia.py
+++ loganomalia.py
@@ -11,17 +11,17 @@ class LogAnomaliaDWCA(object):
     def __init__(self):
         self.log = {}
 
-
     def _setup_termtest_dict(self):
         """create empty dictionary of the format:
             {"ids":[], "sample":[]}
         """
         return {"ids":[], "sample":[]}
 
-    def _check_if_new_failure(self):
+    def _check_if_new_failure(self, row, term, test):
         """check if the failure is different from the previous failures for
         this term-test combination; if so, store the value
         """
+        return row.data[qn(term)] in self.log[term][test]["sample"]
 
     def _add_failure(self, row, term, test):
         """add the row id to the specific term
@@ -34,7 +34,8 @@ class LogAnomaliaDWCA(object):
             self.log[term] = {test : self._setup_termtest_dict()}
 
         self.log[term][test]["ids"].append(row.id)
-        #self.log[term][test][sample] =
+        if not self._check_if_new_failure(row, term, test):
+            self.log[term][test]["sample"].append(row.data[qn(term)])
 
     # EQUAL TO
     def check_equal(self, row, term, value):
