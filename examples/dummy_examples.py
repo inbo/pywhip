5b112b376d1cf67fe0158ea802af834e7a9a68be
stijnvanhoey <stijnvanhoey@gmail.com>
2016-04-28 11:06:00 +0200

    split dummy examples and vlinder example

---
+++ examples/dummy_examples.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 11:04:07 2016
+
+@author: stijn_vanhoey
+"""
+
+import yaml
+from dwca.read import DwCAReader
+from dwcavalidator.validators import DwcaValidator
+
+
+#%% Read the YAML file
+#schema = yaml.load(open('./settings.yaml'))
+
+schema  ="""
+                decimalLatitude:
+                    type : float
+                    min : 50.68
+                    max : 51.51
+                individualCount:
+                    type : integer
+                    min : 2
+         """
+
+testdoc = {'accessRights': u'http://www.inbo.be/en/norms-for-data-use',
+           'decimalLatitude' : '51.55',
+           'individualCount': u'1'}
+
+v = DwcaValidator(yaml.load(schema))
+#v = DwcaValidator(yaml.load(schema))
+#v = DwcaValidator(_preprocess_schema(yaml.load(schema)))
+v.allow_unknown = True
+
+#v.validate(document)
+v.validate(testdoc)
+print(v.errors)
--- examples/vlinders/vlinderdbase.py
+++ examples/vlinders/vlinderdbase.py
@@ -11,34 +11,11 @@ from dwcavalidator.validators import DwcaValidator
 
 
 #%% Read the YAML file
-#schema = yaml.load(open('./settings.yaml'))
+schema = yaml.load(open('./settings.yaml'))
 
-schema  ="""
-                decimalLatitude:
-                    type : float
-                    min : 50.68
-                    max : 51.51
-                individualCount:
-                    type : integer
-                    min : 2
-         """
 
-testdoc = {'accessRights': u'http://www.inbo.be/en/norms-for-data-use',
-           'decimalLatitude' : '51.55',
-           'individualCount': u'1'}
-
-v = DwcaValidator(yaml.load(schema))
-#v = DwcaValidator(yaml.load(schema))
-#v = DwcaValidator(_preprocess_schema(yaml.load(schema)))
+v = DwcaValidator(schema)
 v.allow_unknown = True
-
-#v.validate(document)
-v.validate(testdoc)
-print(v.errors)
-
-
-#%%
-
 
 #%% Read the archive
 errors = {}
