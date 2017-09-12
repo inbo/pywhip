d3c46f861faf80caa0ecd3f4b7f0da1770f5182a
stijnvanhoey <stijnvanhoey@gmail.com>
2017-09-12 15:56:57 +0200

    Fix works locally, travis still error

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ sphinx.environment.BuildEnvironment.warn_node = _warn_node
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
