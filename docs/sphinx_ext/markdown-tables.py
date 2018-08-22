74a8373bdf0c0d67349db3b72e72564af4452520
stijnvanhoey <stijnvanhoey@gmail.com>
2018-08-22 15:01:24 +0200

    Use local version of sphinx table ext

---
+++ docs/sphinx_ext/markdown-tables.py
@@ -0,0 +1,39 @@
+#
+# Code from https://github.com/ryanfox/sphinx-markdown-tables
+# All credits to to @ryanfox
+#
+
+import re
+import markdown
+
+__version__ = '0.0.8'
+
+def setup(app):
+    app.connect('source-read', process_tables)
+    return {'version': __version__}
+
+
+def process_tables(app, docname, source):
+    """
+    Convert markdown tables to html, since recommonmark can't. This requires 3 steps:
+        Snip out table sections from the markdown
+        Convert them to html
+        Replace the old markdown table with an html table
+    This function is called by sphinx for each document. `source` is a 1-item list. To update the document, replace
+    element 0 in `source`.
+    """
+    md = markdown.Markdown(extensions=['markdown.extensions.tables'])
+    table_processor = markdown.extensions.tables.TableProcessor(md.parser)
+
+    raw_markdown = source[0]
+    blocks = re.split(r'\n{2,}', raw_markdown)
+
+    for i, block in enumerate(blocks):
+        if table_processor.test(None, block):
+            html = md.convert(block)
+            styled = html.replace('<table>', '<table border="1" class="docutils">', 1)  # apply styling
+            blocks[i] = styled
+
+    # re-assemble into markdown-with-tables-replaced
+    # must replace element 0 for changes to persist
+    source[0] = '\n\n'.join(blocks)
